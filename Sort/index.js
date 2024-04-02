12948ae0e9a3ccf132129f18f9fd68f8a1dbdb22
AterixGz <tpsbb123@gmail.com>
2024-04-02 23:05:37 +0700

    update

--- Sort/index.js
+++ Sort/index.js
@@ -1,8 +1,9 @@
-// main.js
+
 const bubbleSort = {
     sort: function(arr) {
+        console.time();
         const n = arr.length;
-
+        
         for (let i = 0; i < n - 1; i++) {
             for (let j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -11,40 +12,44 @@ const bubbleSort = {
                 }
             }
         }
-
+        console.timeEnd();
+        
         return arr;
     }
 };
 
 const insertionSort = {
     sort: function(arr) {
+        console.time();
         const n = arr.length;
-
+        
         for (let i = 1; i < n; i++) {
             let key = arr[i];
             let j = i - 1;
-
+            
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j--;
             }
             arr[j + 1] = key;
         }
-
+        
+        console.timeEnd();
         return arr;
     }
 };
 
 const shellSort = {
     sort: function(arr) {
+        console.time();
         const n = arr.length;
         let gap = Math.floor(n / 2);
-
+        
         while (gap > 0) {
             for (let i = gap; i < n; i++) {
                 let temp = arr[i];
                 let j = i;
-
+                
                 while (j >= gap && arr[j - gap] > temp) {
                     arr[j] = arr[j - gap];
                     j -= gap;
@@ -53,30 +58,56 @@ const shellSort = {
             }
             gap = Math.floor(gap / 2);
         }
-
+        
+        console.timeEnd();
         return arr;
     }
 };
 
 let data = document.getElementById("data")
 let result = document.getElementById("result")
+let resultaftersort = document.getElementById("resultAfterSort")
+let sucdata = parseFloat(data.value)
 let info = []
 
   function add(){
-    info.push(data.value)
-    console.log(data.value)
+    info.push(parseFloat(data.value))
+    console.log(parseFloat(data.value))
     data.value = ""
-    result.innerHTML = info
+    result.innerHTML = "ข้อมูล : "+info
+  }
+  function remove(){
+    info.pop(data.value)
+    console.log(info)
+    result.innerHTML = "ข้อมูล : "+info
   }
 
+  function removeAll(){
+    info = []
+    data.value = ""
+    result.innerHTML = ""
+    resultaftersort.innerHTML = ""
+  }
+
+  function randomNumber(){
+    for (let i = 0; i < 10; i++) {
+          let randomnumber = Math.floor(Math.random()*100)
+          info.push(randomnumber)
+        }
+        result.innerHTML = "ข้อมูล : "+info
+    }
+
   function bubble(){
-    console.log(bubbleSort.sort(info))
-  }
-  function insertion(){
-    console.log(insertionSort.sort(info))
-  }
-  function shell(){
-    console.log(shellSort.sort(info))
+      console.log(bubbleSort.sort(info))
+      resultaftersort.innerHTML = "BubbleSort : "+info
+    }
+    function insertion(){
+        console.log(insertionSort.sort(info))
+        resultaftersort.innerHTML = "InsertionSort : "+info
+    }
+    function shell(){
+        console.log(shellSort.sort(info))
+        resultaftersort.innerHTML = "ShellSort : "+info
   }
 
 // // ตัวอย่างการใช้งาน
